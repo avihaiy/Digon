09bdd88814d5bf1901a440db84d7cbd001b4e9b3
avihaiy <avihaidj0@gmail.com>
2026-07-07 08:14:00 +0300

    fix: Fix iOS keyboard white screen bug by moving overflow-x to wrapper

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -260,7 +260,7 @@ export default function AppLayout({ children }: AppLayoutProps) {
       />
 
       {/* Main content */}
-      <div className="flex-1 flex flex-col min-h-screen">
+      <div className="flex-1 flex flex-col min-h-screen w-full max-w-[100vw] overflow-x-hidden">
         {/* Offline Banner */}
         <OfflineBanner />
         
