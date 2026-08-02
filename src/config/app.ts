0bc9418f80cef62ecb36f2152d958603d86d1db2
avihaiy <avihaidj0@gmail.com>
2026-08-02 13:37:38 +0300

    chore: remove Bite Time widget from home page to prevent clutter

--- src/config/app.ts
+++ src/config/app.ts
@@ -6,7 +6,7 @@
 export const APP_CONFIG = {
   name: 'דיגון',
   description: 'מערכת הדייג של ישראל',
-  version: '1.1.40',
+  version: '1.1.41',
   lastUpdated: 'פברואר 2026',
   developer: {
     name: 'Avihai Yosipovich',
